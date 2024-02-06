21e1ed53897319608d42fe8f1024b70c975ac0e8
David O.Silva <srnowdkzinx22@gmail.com>
2024-02-06 16:21:23 -0300

    first commit

---
+++ Fundamentals-01/scrypt.js
@@ -0,0 +1,44 @@
+/* 
+ -------- JavaScrit Fundamentals - Part 1 --------
+*/
+
+// Values and Variables
+
+let country, continent, population;
+
+country = 'Brazil';
+continent = 'South American';
+population = 220000000;
+
+console.log(`Country: ${country}`);
+console.log(`Continent: ${continent}`);
+console.log(`Population: ${population}`);
+
+console.log();
+
+// Data Types
+
+const isIsland = false;
+
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
+console.log(typeof isIsland);
+
+console.log();
+
+// let, const and var
+
+const language = 'Portuguese';
+
+console.log(`Country: ${country}`);
+console.log(`Continent: ${continent}`);
+console.log(`Population: ${population}`);
+console.log(`Is island: ${isIsland}`);
+
+console.log(`Language: ${language}`);
+
+console.log();
+
+// Basic Operators
+
